1e6e624db520dd8fc76ca96877959e4fd3358c2c
EC2 Default User <ec2-user@ip-172-31-40-200.ap-south-1.compute.internal>
2023-04-19 13:13:39 +0000

    h

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1"
-  shared_credentials_files = "/home/ec2-user/.aws/credentials"
+  shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
 }
 
 locals {
