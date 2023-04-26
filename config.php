eac74064b2263788179f7f65ba3b322f42491120
wahtej <125274697+wahtej@users.noreply.github.com>
2023-04-26 10:45:13 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $currency = '₹';
-$db_username = 'root';
-$db_password = '';
+$db_username = 'admin';
+$db_password = 'adminadmin';
 $db_name = 'bolt';
-$db_host = 'localhost';
+$db_host = 'bolt.c66etawe6jmx.ap-south-1.rds.amazonaws.com';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
